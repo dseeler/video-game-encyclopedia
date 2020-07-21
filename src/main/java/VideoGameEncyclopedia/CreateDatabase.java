81f65b1c2c4b576f3176e51721924a4fb9314dc6
seeler24 <das09474@uga.edu>
2020-07-21 03:48:08 -0400

    Maven environment setup. Local SQL server video_game_encyclopedia database creation implemented.

---
+++ src/main/java/VideoGameEncyclopedia/CreateDatabase.java
@@ -0,0 +1,31 @@
+package VideoGameEncyclopedia;
+
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+
+public class CreateDatabase {
+    public static void main(String[] args) {
+        try {
+            // Connect to local SQL server using JDBC
+            String url = "jdbc:mysql://localhost:3306/?useTimezone=true&serverTimezone=UTC";
+            Connection conn = DriverManager.getConnection(url, "root", "Run3scap3"); // Your server password
+
+            // Create connection statement
+            Statement stmnt = conn.createStatement();
+
+            // Create video_game_encyclopedia database on local SQL server with script
+            File file = new File("src/main/resources/video_game_encyclopedia.sql");
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNextLine()) {
+                PreparedStatement update = conn.prepareStatement(fileScanner.nextLine());
+                update.executeUpdate();
+            }
+            fileScanner.close();
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }//end main
+}//end JDBCExample
+
