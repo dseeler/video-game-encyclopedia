141f2ebdfe0c13caf6e74f9055f721359b625da7
Chris Fisher <cfisher@blizzard.com>
2020-07-26 17:57:04 -0400

    client set up

---
+++ client/src/components/app.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './../css/app.css';
+
+const fetchData = () => {
+
+};
+
+const App = () => {
+    const [activeComponent, setActiveComponent] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [data, setData] = useState([]);
+    const [wishList, setWishList] = useState([]);
+
+    return (
+        <>
+            penis
+        </>
+    );
+};
+
+export default App;
